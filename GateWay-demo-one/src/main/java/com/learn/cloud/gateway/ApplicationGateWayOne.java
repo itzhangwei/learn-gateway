a20657849cef2388fc6e2cfe2713962d9266218e
zack.zhang <zhangwei>
2019-08-06 19:49:30 +0800

    通过代码生成RouteLocator类，设置gateway路由拦截

--- GateWay-demo-one/src/main/java/com/learn/cloud/gateway/ApplicationGateWayOne.java
+++ GateWay-demo-one/src/main/java/com/learn/cloud/gateway/ApplicationGateWayOne.java
@@ -3,6 +3,9 @@ package com.learn.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author zack.zhang
@@ -18,5 +21,12 @@ public class ApplicationGateWayOne {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationGateWayOne.class, args);
+    }
+    
+    @Bean
+    public RouteLocator getRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes().route("about-path",
+                r->r.path("/about")
+                        .uri("http://ityouknow.com")).build();
     }
 }
